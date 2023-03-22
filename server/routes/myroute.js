6ab4c4257a2365a0af7f0b25cbd513096c2b6783
admon3 <31572749+admon3@users.noreply.github.com>
2023-03-21 22:59:17 -0400

    Update myroute.js

--- server/routes/myroute.js
+++ server/routes/myroute.js
@@ -11,6 +11,48 @@ module.exports = function(app) {
         // res.send(mypath);
         res.sendFile(mypath);
     });
+    app.get('/HomeAtractivos', function(req, res) {
+        const path = require('path');
+        let mypath= path.join(__dirname, '../..', 'client', 'index.html');
+        // res.send(mypath);
+        res.sendFile(mypath);
+    });
+    app.get('/SiteNoticias', function(req, res) {
+        const path = require('path');
+        let mypath= path.join(__dirname, '../..', 'client', 'index.html');
+        // res.send(mypath);
+        res.sendFile(mypath);
+    });
+    app.get('/SiteHospedajes', function(req, res) {
+        const path = require('path');
+        let mypath= path.join(__dirname, '../..', 'client', 'index.html');
+        // res.send(mypath);
+        res.sendFile(mypath);
+    });
+    app.get('/HomeResenas', function(req, res) {
+        const path = require('path');
+        let mypath= path.join(__dirname, '../..', 'client', 'index.html');
+        // res.send(mypath);
+        res.sendFile(mypath);
+    });
+    app.get('/HomeComoLlegar', function(req, res) {
+        const path = require('path');
+        let mypath= path.join(__dirname, '../..', 'client', 'index.html');
+        // res.send(mypath);
+        res.sendFile(mypath);
+    });
+    app.get('/HomeConociendoCopacabana', function(req, res) {
+        const path = require('path');
+        let mypath= path.join(__dirname, '../..', 'client', 'index.html');
+        // res.send(mypath);
+        res.sendFile(mypath);
+    });
+    app.get('/resenas', function(req, res) {
+        const path = require('path');
+        let mypath= path.join(__dirname, '../..', 'client', 'index.html');
+        // res.send(mypath);
+        res.sendFile(mypath);
+    });
     var User = app.models.User;
     app.post('/login', function(req, res) {
         User.login({
